3a8a19bc975a03b6d2454181fbe883ff13d68c16
Albert <alberthoms@cmail.cat>
2016-08-14 22:29:01 +0200

    Lògica javascrió ocultar-mostrar camps d'Organització al formulari de Projecte

--- modules/custom/transferhub/transferhub_project/js/transferhub_project.js
+++ modules/custom/transferhub/transferhub_project/js/transferhub_project.js
@@ -1,3 +1,30 @@
 /**
  * Created by albert on 13/08/16.
  */
+
+jQuery( document ).ready(function() {
+    
+    //function: update visibility of organization fields, based on user selections
+    function updateDisplay()
+    {
+        if (jQuery("#edit-field-use-existing-organization-value").is(":checked"))
+        {
+            jQuery("#edit-field-organization-new-wrapper").hide();
+            jQuery("#edit-field-organization-existing-wrapper").show();
+        }
+        else
+        {
+            jQuery("#edit-field-organization-existing-wrapper").hide();
+            jQuery("#edit-field-organization-new-wrapper").show();
+        }
+    }
+
+    //inicialization
+    updateDisplay();
+
+    //set event
+    jQuery('#edit-field-use-existing-organization-value').change(function() {
+        updateDisplay();
+    });
+
+});
